1d37b41724158758ed75d3a7c1e1d63b140fa2c5
Naveen Chandran <naveenkchandran@gmail.com>
2013-10-07 01:46:25 -0400

    Menu_Screen with title added

--- MULE Project/src/Menu_Screen.java
+++ MULE Project/src/Menu_Screen.java
@@ -1,5 +1,10 @@
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Font;
+import java.awt.Color;
 
 
 public class Menu_Screen extends JPanel {
@@ -8,12 +13,23 @@ public class Menu_Screen extends JPanel {
 	 * Create the panel.
 	 */
 	public Menu_Screen() {
+		setBackground(Color.BLACK);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] {100};
-		gridBagLayout.rowHeights = new int[] {100};
-		gridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100};
+		gridBagLayout.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
+		
+		JLabel title = new JLabel("MULE THE GAME");
+		title.setForeground(Color.YELLOW);
+		title.setFont(new Font("Stencil", Font.ITALIC, 32));
+		
+		GridBagConstraints gbc_title = new GridBagConstraints();
+		gbc_title.insets = new Insets(0, 0, 5, 5);
+		gbc_title.gridx = 6;
+		gbc_title.gridy = 2;
+		add(title, gbc_title);
 
 	}
 
