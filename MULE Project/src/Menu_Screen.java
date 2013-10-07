da9fd14c2c10c08044e5884eed9d643a839e7abb
Naveen Chandran <naveenkchandran@gmail.com>
2013-10-07 01:38:34 -0400

    Menu Screen with Grid Bag layout

--- MULE Project/src/Menu_Screen.java
+++ MULE Project/src/Menu_Screen.java
@@ -1,4 +1,5 @@
 import javax.swing.JPanel;
+import java.awt.GridBagLayout;
 
 
 public class Menu_Screen extends JPanel {
@@ -7,6 +8,12 @@ public class Menu_Screen extends JPanel {
 	 * Create the panel.
 	 */
 	public Menu_Screen() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] {100};
+		gridBagLayout.rowHeights = new int[] {100};
+		gridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 
 	}
 
