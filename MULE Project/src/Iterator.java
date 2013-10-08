2b10504ff279bd32bcbc1282f455fc59d58db305
Fordro47 <Fordro47@gmail.com>
2013-10-08 01:14:32 -0400

    Iterator change

--- MULE Project/src/Iterator.java
+++ MULE Project/src/Iterator.java
@@ -1,14 +1,23 @@
 
+
+
+
 public class Iterator {
-	static int turn = 1;
-	int numPlayers;
+	Menu_Screen ms = new Menu_Screen();
+	//local instance variables
+	private static int turn = 1;
+	private int numPlayers;
+	
+	//constructor
 	public Iterator(int numPlayers){
+		
 		this.numPlayers = numPlayers;
 		choosePlayer();
 	}
 	public void choosePlayer(){
 		if(turn <= numPlayers){
 			for(int i = turn; i<=numPlayers; i++){
+				//create selection screen, 
 				
 			}
 		}
