075078698762e9e1a39acd711d2f8545d2faf382
Naveen Chandran <naveenkchandran@gmail.com>
2013-10-08 01:45:47 -0400

    Iterator commit

--- MULE Project/src/Iterator.java
+++ MULE Project/src/Iterator.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 public class Iterator {
 	private static int turn = 1;
@@ -25,3 +26,34 @@ public class Iterator {
 		return iterator;
 	}
 }
+=======
+
+
+
+
+public class Iterator {
+	Menu_Screen ms = new Menu_Screen();
+	Screen sc;
+	//local instance variables
+	private static int turn = 1;
+	private int numPlayers;
+	
+	//constructor
+	public Iterator(int numPlayers){
+		
+		this.numPlayers = numPlayers;
+		choosePlayer();
+	}
+	public void choosePlayer(){
+		if(turn <= numPlayers){
+			for(int i = turn; i<=numPlayers; i++){
+				//create selection screen, 
+				sc = new Screen();
+			}
+		}
+	}
+	public void simulateTurn(){
+		//not implemented yet
+	}
+}
+>>>>>>> 0e8c9bf0032048cb8e83733fc575e79b9d7ca7d8
