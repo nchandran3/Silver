55bf1c626f10193a0af144ebf475006036c5fa93
CewlCode <mcarlson9@gatech.edu>
2013-10-08 01:12:35 -0400

    Revert "Eclipse Test Commit"
    This reverts commit 75d840cccc261d9fb8909682acd196ba101cecaa.

--- MULE Project/src/Menu_Screen.java
+++ MULE Project/src/Menu_Screen.java
@@ -5,37 +5,30 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Font;
 import java.awt.Color;
-import javax.swing.SwingConstants;
-import javax.swing.JRadioButton;
 
 
 public class Menu_Screen extends JPanel {
-
-	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Create the panel.
 	 */
 	public Menu_Screen() {
-		setBackground(Color.BLUE);
+		setBackground(Color.BLACK);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100};
-		gridBagLayout.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100};
+		gridBagLayout.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
-		JLabel title = new JLabel("M.U.L.E.");
-		title.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel title = new JLabel("MULE THE GAME");
 		title.setForeground(Color.YELLOW);
-		title.setFont(new Font("Stencil", Font.BOLD, 32));
+		title.setFont(new Font("Stencil", Font.ITALIC, 32));
 		
 		GridBagConstraints gbc_title = new GridBagConstraints();
-		gbc_title.gridheight = 2;
-		gbc_title.gridwidth = 10;
-		gbc_title.insets = new Insets(0, 0, 5, 10);
+		gbc_title.insets = new Insets(0, 0, 5, 5);
 		gbc_title.gridx = 6;
-		gbc_title.gridy = 4;
+		gbc_title.gridy = 2;
 		add(title, gbc_title);
 
 	}
