7451ad63836a3156c41f72d98803f111fed953da
Fordro47 <Fordro47@gmail.com>
2013-10-07 22:34:52 -0400

    New Iterator Class

---
+++ MULE Project/src/Iterator.java
@@ -0,0 +1,19 @@
+
+public class Iterator {
+	static int turn = 1;
+	int numPlayers;
+	public Iterator(int numPlayers){
+		this.numPlayers = numPlayers;
+		choosePlayer();
+	}
+	public void choosePlayer(){
+		if(turn <= numPlayers){
+			for(int i = turn; i<=numPlayers; i++){
+				
+			}
+		}
+	}
+	public void simulateTurn(){
+		
+	}
+}
