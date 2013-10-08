e9478e87c79cf81186dad61e9470ff4105e1f577
Hamilton Greene <hamilton.greene@gmail.com>
2013-10-08 02:16:43 -0400

    Person Interface and Player Class created

---
+++ MULE Project/src/Person.java
@@ -0,0 +1,14 @@
+import java.awt.Point;
+
+
+public interface Person {
+
+		public Point move(Point p);
+		public Point getLocation();
+		
+		public int[] addResources(int[] transaction);
+		
+		public String getRace();
+
+
+}
