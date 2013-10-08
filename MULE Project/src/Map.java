e5bd072c97757bd2d3ed38cef79e6eb126ca8a1f
CewlCode <mcarlson9@gatech.edu>
2013-10-08 03:14:59 -0400

    Edits to Map and Tile
    -Map
    -added tileMap(Tile[][]) and players(Player[])
    -edited the constructor to include the new param for the new players
    -added getTilemap()
    -added getPlayers()
    -Tile
    -added some javadoc
    -added setOwner() - which allows the variable owner to be edited.

--- MULE Project/src/Map.java
+++ MULE Project/src/Map.java
@@ -1,11 +1,21 @@
-import java.util.AbstractMap;
-
+/**
+ * This class is responsible for creating the map and storing the tileMap, as a 2-D 
+ * array, and the players of game, stored in the array players.
+ * @author Michael Carlson
+ *
+ */
 public class Map {
-	Tile[][] map;
+	private Tile[][] tileMap;
+	private Player [] players;
 	
-	public Map(int numTiles){
-		map = new Tile[numTiles][numTiles];
-		
+	public Map(int numTiles, Player[] newPlayers){
+		tileMap = new Tile[numTiles][numTiles];
+		players = newPlayers;
 	}
-	
+	public Tile[][] getTileMap(){
+		return tileMap;
+	}
+	public Player[] getPlayers(){
+		return players;
+	}
 }
--- MULE Project/src/Tile.java
+++ MULE Project/src/Tile.java
@@ -1,24 +1,30 @@
 
+/**
+ * This class represents a Tile on the Map. It stores the tileType(int), owner(Player), and isOwned(boolean).
+ * There are 6 different types of tiles that are given in the constructor. The owner is set with the TileSold
+ * method once the player has chosen a new tile in the GUI of the game.
+ * @author Michael Carlson
+ *
+ */
 public class Tile {
-	/*Instance variables:
-	 * 	-Tile type(stored as a (two character string or numbers?))
-	 * 		-mountain(3 types)
-	 * 		-river
-	 * 		-plain
-	 * 		-Town Center
-	 * 	-isOwned(Boolean: saying if the tile is owned by a player/computer)
-	 *  -Owner(Player: player that owns the tile)
-	 */	
-	private int tileType;
+
+	private int tileType; //make a char?
 	private Player owner;
 	private boolean isOwned;
 	
 	public Tile(int newTileType){
 		tileType = newTileType;
 	}
-	
+	/**
+	 * changes the owner of the Tile to the parameter given.
+	 * @param player: new owner of the tile.
+	 */
 	public void TileSold(Player player){
 		owner = player;
+		isOwned = true;
+	}
+	public void setOwner(Player newOwner){
+		owner = newOwner;
 	}
 	public int getTileType(){
 		
