59ddf05c3a181a25d5efc2caccfdba8993a962c4
CewlCode <mcarlson9@gatech.edu>
2013-10-09 00:48:04 -0400

    Created controller class
    made constructor and createPlayer method.

---
+++ MULE Project/src/Controller.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+
+public class Controller {
+	private Player[] players;
+	
+	public Controller(){
+		
+	}
+	
+	public void createPlayer(String Name, Race race, Color color){
+		Player newPlayer = new Player();
+	}
+}
