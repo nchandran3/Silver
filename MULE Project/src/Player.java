6a750b7c7d682bf5c878c7794d0fefb6cabb362f
Hamilton Greene <hamilton.greene@gmail.com>
2013-10-09 01:58:49 -0400

    Added pseudoCode for future Player features

--- MULE Project/src/Player.java
+++ MULE Project/src/Player.java
@@ -1,31 +1,28 @@
 import java.awt.Color;
 import java.awt.Point;
 
-
+/**
+ * This class is responsible for the player's basic information.
+ * @author Hamilton Greene
+ *
+ */
 public class Player implements Person {
 	
 	private final Race race;
 	private String name;
 	private Color color;
 	private int[] resources;
+	private final Map map;
 	private Point location;
 	
 	Player(String name, Color color, Race race){
 		this.name = name;
-		setColor(color);
+		this.color = color;
 		this.race = race;
 		resources = new int[4];
+		playerInit();
 		location.x = 0;
 		location.y = 0;
-	}
-	
-	/**
-	 * Takes in the string color and converts it back to an int
-	 * where it gives the player the respective color
-	 * @param color
-	 */
-	private void setColor(Color color){
-		this.color = color;
 	}
 	
 	/**
@@ -52,6 +49,34 @@ public class Player implements Person {
 			resources[i]+=transaction[i];
 		}
 		return resources;
+	}
+	
+	/**
+	 * This method will set the player's attributes and starting 
+	 * resources based on map chosen and selected race.
+	 */
+	public void playerInit(){
+		//map = Map.getMapType();
+		//resources[x] moneyStart = 1000;
+		//(where x is chosen slot for money)
+		//if(map==North){
+		//	if(race == Lannister){
+		//		moneyStart = 600;
+		//	}else if(race == Stark){
+		//		moneyStart = 1600;
+		//	}
+		//}else if(map==AcrossSea){
+		//	if(race == Baratheon){
+		//		moneyStart = 600;
+		//	]else if(race == Targaryen){
+		//		moneyStart = 1600;
+		//}else{
+		//	if(race==Targaryen){
+		//		moneyStart = 600;
+		//	}else if(race == Lannister){
+		//		moneyStart == 1600;
+		//	}
+		//}
 	}
 
 	@Override
