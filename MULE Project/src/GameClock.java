0c94ef5ffbd543b8117d0a11b50a8c7316acb149
Hamilton Greene <hamilton.greene@gmail.com>
2013-10-09 03:25:39 -0400

    Worked on GameClock

--- MULE Project/src/GameClock.java
+++ MULE Project/src/GameClock.java
@@ -1,8 +1,34 @@
-
+/**
+ * This class keeps track of turn time.
+ * @author Hamilton Greene
+ *
+ */
 public class GameClock {
 	private static long time;
+	private long startTime;
+	private boolean running;
 	
 	public GameClock(){
-		
+		time = System.currentTimeMillis();
+	}
+	
+	public boolean startTurn() throws InterruptedException{
+		if(!running){
+			startTime();
+		}
+		while(elapsedTime(startTime)<30000){
+			wait(10,1000);
+		}
+		running=false;
+		return false;
+	}
+	
+	public void startTime(){
+		startTime = System.currentTimeMillis();
+		running = true;
+	}
+	
+	public long elapsedTime(long time){
+		return System.currentTimeMillis() - time;
 	}
 }
