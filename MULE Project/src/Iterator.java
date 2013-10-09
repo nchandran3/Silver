0997f00b3f4a2cf7398df804ba694b851b6a14d4
Fordro47 <Fordro47@gmail.com>
2013-10-09 00:15:54 -0400

    Change to iterator
    card layout

--- MULE Project/src/Iterator.java
+++ MULE Project/src/Iterator.java
@@ -1,41 +1,69 @@
-//import java.awt.Color;
 import java.awt.*;
 import java.util.ArrayList;
-
+import javax.swing.*;
 
 public class Iterator {
-	private static int turn = 1;
-	private int numPlayers;
 	private static Iterator iterator;
-	private Player[] plarr;
-	private final Point center = (0, 0);
-	private ArrayList<Integer> colors = new ArrayList<Integer>(6);
-	
-	public Iterator(int numPlayers){
-		this.numPlayers = numPlayers;
-		//choosePlayer(null, null, null, null, null);
-		for(int j = 0; j<colors.size(); j++){
-			colors.add(j);
-		}
-		plarr = new Player[numPlayers];
+	private JFrame frame;
+	private CardLayout c;
+	public Iterator(){iterator = this;}
+	public Iterator(JFrame gameframe){
 		iterator = this;
-		PlayerSelection ps = new PlayerSelection();
-		for(int i = turn; i<=numPlayers; i++){
-			String[] nP = ps.selectPlayer(colors);
-		}
+		frame = gameframe;
+		c = (CardLayout)frame.getLayout();
 	}
-	public void choosePlayer(String name, String color, String race, int[] resources){
-		//plarr.add(new Player(name, color, race, null, CENTER));
-		if(turn <= numPlayers){
-			
-		}
+	public void switchScreen(){
+		c.next(frame);
 	}
-	public void simulateTurn(){
-		//hep
+	public void disposing(){
+		c.removeLayoutComponent(frame);
 	}
-	
 	public static Iterator getIterator()
 	{
 		return iterator;
 	}
 }
+
+
+
+/*
+private static int turn = 1;
+private int numPlayers;
+private static Iterator iterator;
+private Player[] plarr;
+//private final Point center = (0, 0);
+private ArrayList<Integer> colors = new ArrayList<Integer>(6);
+
+public Iterator(){iterator = this;}
+
+public Iterator(int numPlayers){
+	this.numPlayers = numPlayers;
+	plarr = new Player[numPlayers];
+	buildPlayers();
+	for(int j = 0; j<colors.size(); j++){
+		colors.add(j);
+	}
+	
+	iterator = this;
+	PlayerSelection ps = new PlayerSelection();
+	for(int i = turn; i<=numPlayers; i++){
+		String[] nP = ps.selectPlayer(colors);
+	}
+}
+private void buildPlayers() {
+	for(int i = 0; i < plarr.length; i++){
+		
+	}
+	// TODO Auto-generated method stub
+	
+}
+public void choosePlayer(String name, String color, String race, int[] resources){
+	//plarr.add(new Player(name, color, race, null, CENTER));
+	if(turn <= numPlayers){
+		
+	}
+}
+public void simulateTurn(){
+	//hep
+}
+*/
