7b78bde49527bf9e648d91dbd86b98b271bf1d36
nchandran3 <naveenkchandran@gmail.com>
2013-10-25 01:41:13 -0400

    Added PlayerComparator class

---
+++ MULE Project/src/PlayerComparator.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+/**
+ * 
+ */
+
+/**
+ * PlayerComparator for determining which player is ranked higher
+ * @author Naveen Chandran
+ *
+ */
+public class PlayerComparator implements Comparator<Player> {
+
+	@Override
+	public int compare(Player o1, Player o2) {
+		
+	}
+
+	
+}
