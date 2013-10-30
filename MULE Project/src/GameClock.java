cddf6bf2f404a8620b8c0001cc272f0b5cc9d02a
CewlCode <mcarlson9@gatech.edu>
2013-10-30 01:00:35 -0400

    GameClock edits: 	-created resetClock(int time) 	-created resestClock()

--- MULE Project/src/GameClock.java
+++ MULE Project/src/GameClock.java
@@ -24,7 +24,10 @@ public class GameClock extends JPanel{
 		rectangle = new Rectangle(10,10,20,timeForTurn * 2);
 		this.timeForTurn = timeForTurn * 2;
 	}
-	
+	/**
+	 * Paints the yellow box representing the time left.
+	 * 
+	 */
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.yellow);
@@ -43,9 +46,31 @@ public class GameClock extends JPanel{
 			Thread.sleep(500);
 		}
 //		Controller.controller.endTurn();
-		System.out.println("time up");
+		System.out.println("time up" +"\nIt took " + this.elapsedTime(startTime) + " seconds.");
 	}
 	
+	/**
+	 * Resets the rectangle representing the clock with the time given as the parameter.
+	 * @param timeForTurn
+	 */
+	public void resetClock(int timeForTurn){
+		rectangle.height = timeForTurn * 2;
+		rectangle.y = 10;
+		this.timeForTurn = timeForTurn * 2;
+		repaint();
+		startTime = (int) System.currentTimeMillis();
+	}
+	
+	/**
+	 * Resets the clock with the same amount of time as it was 
+	 * originally given when the GameClock was created.
+	 */
+	public void resetClock(){
+		rectangle.height = timeForTurn;
+		rectangle.y = 10;
+		repaint();
+		startTime = (int) System.currentTimeMillis();
+	}
 	/**
 	 * Starts clock, and sets running to true.
 	 */
@@ -68,15 +93,15 @@ public class GameClock extends JPanel{
 	 * @return
 	 */
 	public int elapsedTime(int time){
-		return (int) System.currentTimeMillis() - time;
+		return (((int) System.currentTimeMillis() - time)/1000);
 	}
 	public static void main(String [] args) throws InterruptedException{
 		GameClock clock = new GameClock(10);
 		clock.startTime();
-		Thread.sleep(2000);
-		System.out.println(clock.elapsedTime(clock.startTime));
-		Thread.sleep(5000);
-		System.out.println(clock.gameTime());
+//		Thread.sleep(2000);
+//		System.out.println(clock.elapsedTime(clock.startTime));
+//		Thread.sleep(5000);
+//		System.out.println(clock.gameTime());
 		javax.swing.JFrame frame = new javax.swing.JFrame();
 //		frame.setLayout(new BorderLayout());
 		frame.setSize(300, 200);
@@ -86,6 +111,10 @@ public class GameClock extends JPanel{
 		frame.setLocation(new Point(100,0));
 		frame.pack();
 		frame.setVisible(true);
+		clock.startCountdown();
+//		clock.resetClock(30);
+//		clock.startCountdown();
+		clock.resetClock();
 		clock.startCountdown();
 
 	}
