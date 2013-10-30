3398cc1d856c2fba8d097de8c4d41bf46ca81b01
nchandran3 <naveenkchandran@gmail.com>
2013-10-30 04:40:41 -0400

    TestTimer class as an example for GameClock

---
+++ MULE Project/src/TestTimer.java
@@ -0,0 +1,105 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import control.GTools;
+
+
+public class TestTimer extends Screen implements ActionListener{
+	private int time;
+	double y;
+	private double rate;
+	private int height = super.height;
+	private final double FLOW = .01;
+	private Timer timer;
+	boolean paused;
+	
+	public TestTimer(int time)
+	{
+		super();
+		setPreferredSize(new Dimension(super.width/8,height));
+		setBackground(Color.BLACK);
+		
+		paused = false;
+		y=0;
+		this.time = time;
+		rate = (height/time*FLOW); //how much to take off the rectangle in order to get blank at the end
+		timer = new Timer((int)(FLOW*1000), this);
+		JButton button = new JButton ("Pause");
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				if(!paused)
+				{
+					pause();
+					System.out.println("The time remaining is " + getTimeRemaining());
+					paused = true;
+					
+				}
+				else
+				{
+					timer.start();
+					paused = false;
+				}
+			}
+		});
+		GTools.positionAndAdd(button, .9, .2, this);
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.setColor(Color.YELLOW);
+		g.fillRect(0, (int)y, 100, height);
+	}
+
+	public void start()
+	{
+		timer.start();
+	}
+	public void pause()
+	{
+		timer.stop();
+	}
+	
+	public int getElapsedTime()
+	{
+		return (int) (y/height * time);
+	}
+	public int getTimeRemaining()
+	{
+		return time - (int)(y/height * time);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(y< height)
+		{
+			y+= rate;
+			repaint();
+		}
+		else
+		{
+			timer.stop();
+			Controller.getController().endTurn();
+		}
+	}
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		TestTimer timer = new TestTimer(100);
+		frame.add(timer);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
+}
