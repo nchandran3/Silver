e7e7b3a65fcebc1aab0202efc3147404ffa8a7bc
CewlCode <mcarlson9@gatech.edu>
2013-10-29 22:45:29 -0400

    Added class

---
+++ MULE Project/src/LandSelectionPhase.java
@@ -0,0 +1,8 @@
+
+public class LandSelectionPhase extends RoundPhase {
+
+	public int getLandPrice() {
+		return 0;
+	}
+
+}
---
+++ MULE Project/src/RoundPhase.java
@@ -0,0 +1,8 @@
+
+public abstract class RoundPhase {
+	protected int landPrice;
+	public RoundPhase(){
+		
+	}
+//	public abstract int getLandPrice();
+}
