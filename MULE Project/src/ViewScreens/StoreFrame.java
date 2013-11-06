315fd94feb65b5ae458f9307fec3c2f7ea2a0e26
nchandran3 <naveenkchandran@gmail.com>
2013-11-06 02:11:22 -0500

    StoreFrame to hold the tabbed Store panels

---
+++ MULE Project/src/ViewScreens/StoreFrame.java
@@ -0,0 +1,68 @@
+package ViewScreens;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author Naveen Chandran
+ *
+ */
+public final class StoreFrame extends JFrame {
+	private JTabbedPane tabs;
+	
+	private StoreFrame()
+	{
+		setLayout(new BorderLayout());
+		
+		tabs = new JTabbedPane();
+		
+		//add the tabs (mule, food, dragonfire, ore)
+		
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		pack();
+	}
+	
+	
+	public static StoreFrame frame()
+	{
+		return Holder.FRAME;
+	}
+	
+	public void show()
+	{
+		setVisible(true);
+	}
+	
+	public void hide()
+	{
+		setVisible(false);
+	}
+	
+	/**
+	 * Singleton Holder class using Bill Pugh implementation
+	 *
+	 */
+	private static class Holder
+	{
+		public static final StoreFrame FRAME = new StoreFrame(); 
+	}
+
+	 
+	private class GoldPanel extends JPanel
+	{
+		public GoldPanel()
+		{
+			this.setBackground(Color.BLACK);
+			this.setPreferredSize(new Dimension(100, 50));
+			this.add(new JLabel("GOLD : " + ))
+		}
+	}
+}
