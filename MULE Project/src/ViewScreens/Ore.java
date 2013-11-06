c5b88c710c076f63b815889c3e0e823541f1c89b
Fordro47 <Fordro47@gmail.com>
2013-11-06 04:12:12 -0500

    Finished Ore

--- MULE Project/src/ViewScreens/Ore.java
+++ MULE Project/src/ViewScreens/Ore.java
@@ -1,29 +1,31 @@
 package ViewScreens;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
 import control.*;
 public class Ore extends StoreScreen {
-
+	public int getSupply()
+	{
+		return store.getOre();
+	}
 	/**
 	 * Needs to set up the quantity box to have options 0 - storeInventory of the resource
 	 */
 	void setUpQuantityBox() {
-		Integer[] options = new Integer[Store.getStore().getOre()];
-		for(int i =0; i <Store.getStore().getOre(); i++ )
+		Integer[] options = new Integer[getSupply()+1];
+		for(int i =0; i <= store.getDragonFire(); i++ )
 			options[i]= i;
 		
 		selector = new JComboBox<Integer>(options);
-		// TODO Auto-generated method stub
-		
 	}
 
 	/**
 	 * The price label should display "Price " + resource's price
 	 */
 	void setUpPriceLabel() {
-		price = new JLabel("Price of Ore" + Store.getStore().getOrePrice());
+		price = new JLabel("Price: " + store.getOrePrice());
 		
 		// TODO Auto-generated method stub
 		
@@ -33,7 +35,7 @@ public class Ore extends StoreScreen {
 	 * The storeInventory label should display the amount of resources remaining in the store of the given resource
 	 */
 	void setUpInventoryLabel() {
-		storeInventory = new JLabel("There are: " + Store.getStore().getOre() + " remaining in the store.");
+		storeInventory = new JLabel("Store Inventory: " + store.getOre());
 		// TODO Auto-generated method stub
 		
 	}
@@ -42,7 +44,7 @@ public class Ore extends StoreScreen {
 	 * Resource Image should be a picture of the current resource. The size should also be set.
 	 */
 	void setUpResourceImage() {
-		// TODO Auto-generated method stub
+		resource_image = new JLabel(new ImageIcon("./Images/mountain.png"));
 		
 	}
 
@@ -61,7 +63,7 @@ public class Ore extends StoreScreen {
 	 * mouseClicked method that should occur when the buy button is pressed
 	 */
 	void buyButtonPressed() {
-		// TODO Auto-generated method stub
+		store.buyOre((int)selector.getSelectedItem());
 		
 	}
 
@@ -69,7 +71,13 @@ public class Ore extends StoreScreen {
 	 * mouseClicked method that should occur when the sell button is pressed
 	 */
 	void sellButtonPressed() {
-		// TODO Auto-generated method stub
+		store.sellOre((int)selector.getSelectedItem());
+		
+	}
+
+	@Override
+	void setUpQuantityBoxLabel() {
+		selectorLabel = new JLabel("Amount of Ore to Buy / Sell");
 		
 	}
 
