9b9aa110b2aec6a71f4f050f2dfbdb1d916f3627
nchandran3 <naveenkchandran@gmail.com>
2013-11-13 05:49:18 -0500

    Start Screen in main that decides to load or start a new game. Implemented in Main

--- MULE Project/src/Game/Main.java
+++ MULE Project/src/Game/Main.java
@@ -1,6 +1,9 @@
 package Game;
 import java.awt.EventQueue;
 
+import javax.swing.JOptionPane;
+
+import ViewScreens.StartScreen;
 import control.*;
 
 
@@ -14,17 +17,28 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GameFrame frame = new GameFrame();
-					Iterator iterator = new Iterator(frame);
-					Controller controller = new Controller();
-				} catch (Exception e) {
-					e.printStackTrace();
+		new StartScreen();
+		while(StartScreen.chosen == 0) {System.out.print("");}
+
+		if(StartScreen.chosen == 1)			//create a new game
+		{
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						GameFrame frame = new GameFrame();
+						Iterator iterator = new Iterator(frame);
+						Controller controller = new Controller();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
-			}
-		});
+			});
+		}
+		
+		else 
+		{
+			
+		}
 	}
 
 }
