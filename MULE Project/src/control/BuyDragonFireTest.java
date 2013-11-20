869c23fd85b7011e2a001597ee4863bd433cfbb5
treyblanden <trey.blanden@gmail.com>
2013-11-20 00:00:28 -0500

    JUnit test for buyDragonFire() updated

--- MULE Project/src/control/BuyDragonFireTest.java
+++ MULE Project/src/control/BuyDragonFireTest.java
@@ -2,16 +2,63 @@ package control;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
-public class BuyDragonFireTest {
+import Player.Player;
+import Player.Race;
 
+public class BuyDragonFireTest {
+	
+	//Player has sufficient funds and asked for proper amount of dragonfire
 	@Test
-	public void testBuyDragonFire() {
+	public void testBuyDragonFireTrue() {
 		Store store = Store.getStore();
-		store.setDragonFirePrice(20);
-		store.setFoodPrice(10);
-		store.setOrePrice(15);
+		
+		Controller controller = Controller.getController();
+		controller.setNumPlayers(1);
+		controller.createPlayer("Trey", Color.BLUE, Race.BARATHEON);
+		
+		controller.getCurrentPlayer().setGold(1000);
+		
+		//Amount of dragonfire allocated to store
+		int amount = store.getDragonFire();
+		
+		boolean result = store.buyDragonFire(amount-3);
+		assertTrue("The transaction was succesful", result);
+	}
+	
+	//Player has insufficient funds but asks for proper amount of dragonfire
+	@Test
+	public void testBuyDragonFireFalse() {
+		Store store = Store.getStore();
+		
+		Controller controller = Controller.getController();
+		controller.setNumPlayers(1);
+		controller.createPlayer("Trey", Color.BLUE, Race.BARATHEON);
+		
+		controller.getCurrentPlayer().setGold(10);
+		
+		int amount = store.getDragonFire();
+		
+		boolean result = store.buyDragonFire(amount-3);
+		assertFalse("The transaction failed", result);
+	}
+	
+	//Player asks for more dragonfire than available
+	@Test
+	public void testBuyDragonFireFalse2() {
+		Store store = Store.getStore();
+		
+		Controller controller = Controller.getController();
+		controller.setNumPlayers(1);
+		controller.createPlayer("Trey", Color.BLUE, Race.BARATHEON);
+		
+		int amount = store.getDragonFire();
+		
+		boolean result = store.buyDragonFire(amount+1);
+		assertFalse("The transaction failed", result);
 	}
 
 }
